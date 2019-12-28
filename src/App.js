f16e8159a311cbc964f5ec13d2f58d8879fb5b9c
Olanrewaju A. Olaboye <eyiwumi.olaboye@gmail.com>
2019-12-28 22:22:42 +0100

    mdbreat bug

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {MDBBox} from 'mdbreact';
+import Box from './components/Box';
 
 function App() {
   return (
     <React.Fragment>
-      <MDBBox display='flex' alignSelf='center'>
+      <MDBBox display='flex' alignSelf='center' color='pink'>
         MDB React Added to the project
       </MDBBox>
-      <MDBBox display="flex" justifyContent="end">
+      <MDBBox display="flex" justifyContent="end" color='red'>
         d-flex justify-content-end
       </MDBBox>
+      <Box/>
     </React.Fragment>
   );
 }
