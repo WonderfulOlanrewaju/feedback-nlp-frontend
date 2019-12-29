538a71d2a4aef5cb0e91a367ea38a2d55da785de
Olanrewaju A. Olaboye <eyiwumi.olaboye@gmail.com>
2019-12-29 19:25:29 +0100

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import {MDBBox} from 'mdbreact';
-import Box from './components/Box';
+import './output.css';
+import Feedbackicon from './feedback.svg';  
+import {Form, Formik} from 'formik';
 
 function App() {
   return (
     <React.Fragment>
-      <MDBBox display='flex' alignSelf='center' color='pink'>
-        MDB React Added to the project
-      </MDBBox>
-      <MDBBox display="flex" justifyContent="end" color='red'>
-        d-flex justify-content-end
-      </MDBBox>
-      <Box/>
+      <div className='container'>
+        <div className='Left-Div'>
+          <div className='mx-auto'>
+            <div className='Feedback-Logo'>Feedback.io</div>
+            <div className='Feedback-Icon'>            
+              <img  src={Feedbackicon} alt='Feedback icon' />
+            </div>
+            <div className='Feedback-Lead'>We would love your feedback.</div>
+            <p>It should only take a couple of minute to fill in your feedbacks.</p>
+          </div>
+        </div>
+        <div>
+            <Formik>
+
+            </Formik>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
