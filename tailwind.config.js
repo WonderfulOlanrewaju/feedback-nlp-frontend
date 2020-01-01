898b1c1bd4bd3b06db82b8d2f377c240b48819dc
Olanrewaju A. Olaboye <eyiwumi.olaboye@gmail.com>
2020-01-01 23:06:07 +0100

    frontend auto

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,12 @@ module.exports = {
       container: {
         marginBottom : '0'
       },
+      minWidth: {
+          '0': '0',
+          '1/4': '25%',
+          '1/2': '50%',
+          '3/4': '75%',
+          'full': '100%',
+        }
     },
   }
